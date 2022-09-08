296296be8b38ee02aa1b378d6b083fc5b7617488
Ali Amjad <66186390+Ali-A-Koye@users.noreply.github.com>
2022-09-08 21:54:30 +0300

    Our Main Entry

--- lib/index.ts
+++ lib/index.ts
@@ -1,7 +1,20 @@
-const entry = () => {
+import IndexType from "../types/index";
+import { Request as ExpressRequest } from "express";
+import { FastifyRequest } from "fastify";
+import validate from "./validator/index";
+const entry = (field: IndexType, req: ExpressRequest | FastifyRequest) => {
 
-    console.log('Hello World');
+  let value = validate(req.body[field]);
 
+  console.log(value);
+  switch (value) {
+    case "url":
+      break;
+    case "base64":
+      break;
+    case "string":
+      break;
+  }
 };
 
 export default entry;

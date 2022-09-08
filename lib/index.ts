084fd9b22a5f3cd07fad4ea51f5af563c8c6d152
Ali Amjad <66186390+Ali-A-Koye@users.noreply.github.com>
2022-09-08 22:57:04 +0300

    Base64 to Buffer and URL to Buffer

--- lib/index.ts
+++ lib/index.ts
@@ -2,16 +2,15 @@ import IndexType from "../types/index";
 import { Request as ExpressRequest } from "express";
 import { FastifyRequest } from "fastify";
 import validate from "./validator/index";
-const entry = (field: IndexType, req: ExpressRequest | FastifyRequest) => {
+import base64ToBuffer from "./base64";
+import urlToBuffer from "./url";
+const entry = async (field: IndexType, req: ExpressRequest | FastifyRequest) => {
 
   let value = validate(req.body[field]);
 
-  console.log(value);
   switch (value) {
-    case "url":
-      break;
-    case "base64":
-      break;
+    case "url": req.body[field] = await urlToBuffer(req.body[field]);break;
+    case "base64": req.body[field] = base64ToBuffer(req.body[field]);break;
     case "string":
       break;
   }
